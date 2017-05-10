ac9809fcb4612d85c0d194247e09fcb3cec9d6ec
Naomi Villanueva <naovillaj@gmail.com>
2017-05-10 02:14:50 -0500

    information about

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
-/*
- * Archivo principal de JS
- */
+var imagenClick = document.getElementById("modal");
+var imagenes = document.getElementsByClassName("modal-click");
+var x = document.getElementById("close");
+var boton = document.getElementsByClassName("modal-button")[0];
+var imagenes = document.getElementsByClassName("modal-click");
+var img = document.getElementById("image-modal");
+var body = document.getElementById("body");
+
+for(var i = 1; i<=imagenes.length; i++){
+	imagenes[i-1].addEventListener("click", function(e){
+		e.preventDefault();
+		imagenClick.classList.toggle("active");
+		body.setAttribute("style", "overflow:hidden");
+    img.setAttribute("src", this.src);
+	})
+}
+
+x.addEventListener("click", function(){
+	imagenClick.classList.toggle("active");
+	body.setAttribute("style", "overflow:scroll");
+})
+
+boton.addEventListener("click", function(){
+	imagenClick.classList.toggle("active");
+	body.setAttribute("style", "overflow:scroll");
+})
